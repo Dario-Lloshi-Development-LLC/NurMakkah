0532f13c090bc68ee7908069ef8ad2c9e10313b4
lloshidario2025-dev <lloshidario2025@gmail.com>
2025-12-30 20:58:28 +0000

    Add Gradle wrapper files for version 8.0.1
    - Created gradle-wrapper.properties to specify the distribution URL and settings.
    - Added gradlew script for POSIX-compliant shell to facilitate Gradle execution.

--- react-native.config.js
+++ react-native.config.js
@@ -2,10 +2,7 @@ module.exports = {
   dependencies: {
     "react-native-vector-icons": {
       platforms: {
-        ios: {
-          sourceDir: "../node_modules/react-native-vector-icons/Fonts",
-          project: "ios/HajjApp.xcodeproj",
-        },
+        ios: null,
       },
     },
     "react-native-fast-image": {
@@ -18,15 +15,4 @@ module.exports = {
     },
   },
   assets: ["./src/assets/fonts/"],
-  commands: {
-    // Custom build commands for production
-    "build:android-prod": [
-      "cd android && ./gradlew assembleRelease",
-      "cd android && ./gradlew bundleRelease",
-    ],
-    "build:ios-prod": [
-      "cd ios && xcodebuild -workspace HajjApp.xcworkspace -scheme HajjApp -configuration Release -destination generic/platform=iOS -archivePath HajjApp.xcarchive archive",
-      "cd ios && xcodebuild -exportArchive -archivePath HajjApp.xcarchive -exportOptionsPlist ExportOptions.plist -exportPath ./build",
-    ],
-  },
 };
