1c5827fc3987cff08e9498e31b51157318f178c1
lloshidario2025-dev <lloshidario2025@gmail.com>
2026-01-04 19:48:54 +0000

    chore(ci): normalize workflow paths & package names; update content checks

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import {AppRegistry} from 'react-native';
-import App from '../App';
+import React from "react";
+import { AppRegistry } from "react-native";
+import App from "../App";
 
-AppRegistry.registerComponent('NurMakkah', () => App);
+AppRegistry.registerComponent("NurMakkah", () => App);
 
 export default App;
